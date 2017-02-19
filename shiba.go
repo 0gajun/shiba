bba9b24f94c8490ca49aa835b6860b885f2e49bc
Junya Ogasawara <oga.ivc.s27@gmail.com>
2017-02-19 21:31:04 +0900

    Print colored

--- shiba.go
+++ shiba.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, world")
+	rect := "\u2588"
+	printMost(rect)
+	printMore(rect)
+	printMedium(rect)
+	printLess(rect)
+	printNone(rect)
+}
+
+func printMost(txt string) {
+	printColored(txt, 22)
+}
+
+func printMore(txt string) {
+	printColored(txt, 28)
+}
+
+func printMedium(txt string) {
+	printColored(txt, 34)
+}
+
+func printLess(txt string) {
+	printColored(txt, 40)
+}
+
+func printNone(txt string) {
+	printColored(txt, 250)
+}
+
+func printColored(txt string, colorCode int16) {
+	fmt.Printf("\033[38;5;%dm%s\033[0m\n", colorCode, txt)
 }
